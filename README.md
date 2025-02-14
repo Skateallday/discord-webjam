8ac097a60b6fd3e64db9a683043c0da009267413
Skateallday <git@stackblitz.com>
2025-02-14 15:48:05 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# discord-webjam
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Skateallday/discord-webjam)
---
+++ src/components/body/body.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { AppComponent } from '../../components/app/app.component';
+
+@Component({
+  selector: 'app-body',
+  imports: [SidebarComponent, AppComponent],
+  templateUrl: `./body.component.html`,
+  standalone: true,
+})
+export class BodyComponent {}
